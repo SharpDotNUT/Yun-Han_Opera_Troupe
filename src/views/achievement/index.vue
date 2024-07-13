05b2ad4e9e6a5e5e86c37ff067fc4b97c3eaaad9
CNChestnut <liyufeng0628@outlook.com>
2024-07-13 16:40:47 +0800

    v0.7.1

--- src/views/achievement/index.vue
+++ src/views/achievement/index.vue
@@ -20,17 +20,32 @@ Data.data.forEach(achievementGoal => {
         });
     }
 });
-console.log(userData.value)
+
+const userUIAF = ref()
+function handleUIAFUpload(file) {
+    const reader = new FileReader()
+    reader.readAsText(file.file)
+    reader.onload = function () {
+        const list = JSON.parse(reader.result).list
+        userUIAF.value = {}
+        for (const achievement of list) {
+            userUIAF.value[achievement.id] = achievement
+        }
+
+        _log(userUIAF.value)
+
+    }
+}
 
 const selectedGoal = ref(0)
 
-function searchAchievement(name,platform){
+function searchAchievement(name, platform) {
     let url
-    if(platform == 'bilibili'){
-        url = `https://search.bilibili.com/all?keyword=${'成就 '+name}`
+    if (platform == 'bilibili') {
+        url = `https://search.bilibili.com/all?keyword=${'原神 成就 ' + name}`
     }
-    if(platform == 'miyoushe'){
-        url = `https://www.miyoushe.com/ys/search?keyword=${'成就 '+name}`
+    if (platform == 'miyoushe') {
+        url = `https://www.miyoushe.com/ys/search?keyword=${'成就 ' + name}`
     }
     window.open(url)
 }
@@ -38,30 +53,46 @@ function searchAchievement(name,platform){
 </script>
 
 <template>
+    <var-uploader accept="application/json" @after-read="handleUIAFUpload">
+        <var-button type="primary" size="large">导入你的 UIAF 数据</var-button>
+    </var-uploader>
     <div class="achievement-container">
-        <div class="achievement-goal-list">
-            <div v-for="(goal, index) in Data.data" :key="index" @click="selectedGoal = index">
-                {{ goal?.name }}
-            </div>
+        <div class="achievement-goal-list var-elevation--3">
+            <var-cell border ripple v-for="(goal, index) in Data.data" :key="index" @click="selectedGoal = index">
+                <p style="font-size:120%">{{ goal?.name }}</p>
+                <p>{{ goal.achievements.length }} 个成就组 | {{ goal.number }} 个成就</p>
+            </var-cell>
         </div>
-        <div class="achievement-list">
+        <div class="achievement-group-list var-elevation--3">
             <div v-if="Data.data[selectedGoal]">
                 <div v-for="(achievementGroup, index) in Data.data[selectedGoal].achievements" :key="index"
-                    class="achievement-group-list">
-                    <div v-for="(achievement, index) in achievementGroup" :key="index"
-                        class="achievement-item">
+                    class="achievement-group-list-item">
+                    <div v-for="(achievement, index) in achievementGroup" :key="index">
                         <h3>{{ achievement.name }}</h3>
-                        <h4>{{ achievement.description }}</h4>
-                        <p>版本 : {{ achievement.version }} ； 奖励 : {{ achievement.rewards }} 原石</p>
-                        <div class="achievement-actions">
-                            <button @click="searchAchievement(achievement.name,'bilibili')">
-                                在哔哩哔哩搜索
-                            </button>
-                            <button @click="searchAchievement(achievement.name,'miyoushe')">
-                                在米游社搜索
-                            </button>
+                        <h5>{{ achievement.description }}</h5>
+                        <p style="color:rgb(0,0,0, 0.7)">
+                            更新版本 {{ achievement.version }} | 奖励 {{ achievement.rewards }} 原石</p>
+                        <div v-if="userUIAF?.[achievement.id]">
+                            <p>{{ (userUIAF[achievement.id].status > index) ? '已达成' : '未达成' }}</p>
+                            <p v-if="userUIAF[achievement.id].status > index">达成时间 {{ new
+                                Date(userUIAF[achievement.id].timestamp * 1000).toLocaleString() }}</p>
+                            <var-progress style="margin-top: 5px;" :value="userUIAF[achievement.id].current / achievement.progress * 100 ||
+                                userUIAF[achievement.id].status > index ? 100 : 0"></var-progress>
                         </div>
+                        <div v-else>
+                            <p>未达成或数据缺失</p>
+                        </div>
+                        <hr style="margin: 10px 0;color:rgb(0,0,0,0.5)">
                     </div>
+                    <var-space justify="flex-end">
+                        <span class="achievement-actions"
+                            @click="searchAchievement(achievementGroup[0].name, 'bilibili')">
+                            在哔哩哔哩搜索
+                        </span><span class="achievement-actions"
+                            @click="searchAchievement(achievementGroup[0].name, 'miyoushe')">
+                            在米游社搜索
+                        </span>
+                    </var-space>
                 </div>
             </div>
         </div>
