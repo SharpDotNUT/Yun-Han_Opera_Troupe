327aeaaed9c659468e3775a9e1faaf51c16ae467
CNChestnut <liyufeng0628@outlook.com>
2024-08-10 23:23:40 +0800

    临时首页

--- src/stores/main.js
+++ src/stores/main.js
@@ -6,11 +6,11 @@ import { Themes, StyleProvider } from "@varlet/ui";
 export const useMainStore = defineStore("main", () => {
   const host_name = "http://localhost:62801";
 
-  const title_text = ref("云翰工具箱");
+  const title_text = ref("云翰社");
   const title = ref("");
   function setTitle(text) {
     title_text.value = text;
-    title.value = "云翰工具箱";
+    title.value = "云翰社";
     title.value += title_text.value ? ` - ${title_text.value}` : "";
     document.title = title.value;
   }
--- src/views/index.vue
+++ src/views/index.vue
@@ -8,66 +8,77 @@ useMainStore().setTitle('首页')
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-const title_raw = '云翰工具箱'
-let title_start = new Date().getTime()
+function randomFloat(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 const subtitle_raw = '一个在 Github 开源的米家游戏工具箱'
 let subtitle_start = new Date().getTime()
 const subtitle = ref('')
 const title = ref('')
 function typing() {
   const delay = 200
-  if (new Date().getTime() - title_start > title_raw.length * delay + 10 * delay) {
-    title_start = new Date().getTime()
-  }
   if (new Date().getTime() - subtitle_start > subtitle_raw.length * delay + 10 * delay) {
     subtitle_start = new Date().getTime()
   }
-  title.value = title_raw.slice(0, Math.floor((new Date().getTime() - title_start) / delay)) + '_'
   subtitle.value = subtitle_raw.slice(0, Math.floor((new Date().getTime() - subtitle_start) / delay)) + '_'
   requestAnimationFrame(typing)
 }
 requestAnimationFrame(typing)
 
 const canvas = ref(null)
+let id_rAF = undefined
 onMounted(() => {
   const ctx = canvas.value.getContext('2d')
   const dpr = window.devicePixelRatio || 1
-  const rect = canvas.value.getBoundingClientRect()
-  // 设置 canvas 的实际尺寸
-  canvas.value.width = rect.width * dpr
-  canvas.value.height = rect.height * dpr
-  // 缩放 canvas 以适应 CSS 样式
-  ctx.scale(dpr, dpr)
-  const width = canvas.value.width / dpr
-  const height = canvas.value.height / dpr
-  const _distance = (width ** 2 + height ** 2) ** 0.5 * 0.25;
+  let rect, width, height, _distance;
+  function init() {
+    rect = canvas.value.getBoundingClientRect()
+    // 设置 canvas 的实际尺寸
+    canvas.value.width = rect.width * dpr
+    canvas.value.height = rect.height * dpr
+    // 缩放 canvas 以适应 CSS 样式
+    ctx.scale(dpr, dpr)
+    width = canvas.value.width / dpr
+    height = canvas.value.height / dpr
+    _distance = (width ** 2 + height ** 2) ** 0.5 * 0.25;
+  }
+  init()
+  const numberOfPoints = width * height / 30000;
+  console.log(numberOfPoints)
   class Point {
     constructor(width, height) {
       this.x = randomInt(0, width);
       this.y = randomInt(0, height);
-      this.vx = randomInt(-5, 5);
-      this.vy = randomInt(-5, 5);
-      this.timeFromLastFrame = undefined;
+      this.r = 2;
+      this.vx = randomFloat(-50, 50);
+      this.vy = randomFloat(-50, 50);
+      this.timeOfLastFrame = Date.now();
     }
     draw(ctx) {
-      this.x += this.vx;
-      this.y += this.vy;
-      if (this.x < 0 || this.x > width) {
+      const theme = useMainStore().theme
+      this.x += this.vx * (Date.now() - this.timeOfLastFrame) / 1000;
+      this.y += this.vy * (Date.now() - this.timeOfLastFrame) / 1000;
+      this.timeOfLastFrame = Date.now();
+      if (this.x < this.r || this.x > width - this.r) {
         this.vx = -this.vx;
       }
-      if (this.y < 0 || this.y > height) {
+      if (this.y < this.r || this.y > height - this.r) {
         this.vy = -this.vy;
       }
       ctx.beginPath()
-      ctx.arc(this.x, this.y, 3, 0, 2 * Math.PI)
+      ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI)
+      ctx.fillStyle = theme === 'dark' ? '#fff' : '#000'
       ctx.fill()
     }
   }
   const points = [];
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < numberOfPoints; i++) {
     points.push(new Point(width, height));
   }
   function draw() {
+    init()
+    const theme = useMainStore().theme
     ctx.clearRect(0, 0, width, height);
     for (let i = 0; i < points.length; i++) {
       points[i].draw(ctx);
@@ -80,7 +91,7 @@ onMounted(() => {
           ctx.moveTo(points[i].x, points[i].y)
           ctx.lineTo(points[j].x, points[j].y)
           ctx.closePath()
-          ctx.strokeStyle = `rgba(0,0,0,${(1 - distance / _distance)})`;
+          ctx.strokeStyle = theme === 'dark' ? `rgba(255,255,255,${(1 - distance / _distance)})` : `rgba(0,0,0,${(1 - distance / _distance)})`;
           ctx.stroke()
         }
       }
@@ -96,11 +107,11 @@ onMounted(() => {
   <div id="main">
     <canvas id="canvas" ref="canvas"></canvas>
     <div id="head">
-      <h1>{{ title }}</h1>
+      <h1>云翰社</h1>
+      <h2>Yun-Han Opera Troupe | <ruby>雲翰社<rt>うんかんしゃ</rt></ruby></h2>
       <p>{{ subtitle }}</p>
     </div>
     <div>
-      <p>新版首页未完成，请使用右上角菜单使用。</p>
       <p>「云婵娟来花婵娟，风流尽在山水间。」</p>
     </div>
   </div>
@@ -108,7 +119,10 @@ onMounted(() => {
 
 <style scoped>
 #main {
-  padding: 10vmin 0px;
+  position: fixed;
+  top: var(--app-bar-height);
+  left: 0;
+  right: 0;
 
   * {
     text-align: center;
@@ -116,12 +130,21 @@ onMounted(() => {
 }
 
 #canvas {
-  width: 100%;
-  height: 100%;
-  display: fixed;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: calc(100vh - var(--app-bar-height));
+  background-color: var(--color-body);
+  filter: blur(0.5px)
 }
 
 #head {
+  position: fixed;
+  top: 50vh;
+  left: 50vw;
+  transform: translate(-50%, -50%);
+
   h1 {
     background: linear-gradient(to bottom,
         #3d3a5b,
@@ -129,12 +152,16 @@ onMounted(() => {
         #72c8d6);
     background-clip: text;
     color: transparent;
-    font-size: 5vmin;
+    font-size: 5vh;
+  }
+
+  h2 {
+    font-size: 2vh;
     margin-bottom: 10px;
   }
 
   p {
-    font-size: 2vmin;
+    font-size: 3vh;
     font-weight: bold;
   }
 }
